2a11f9a468324fdd4df05584f298ffa756900ffe
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-08 19:59:24 -0300

    Add files via upload

---
+++ src/Cliente.java
@@ -0,0 +1,32 @@
+
+public class Cliente {
+
+	private String nome;
+	private String cpf;
+	private String rg;
+	private String contato;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+
+	
+	
+	
+	
+	
+	
+}
