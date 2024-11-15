659c92a3a76200eb217c746f6c1accb26bc9b43e
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2024-11-15 05:00:26 -0300

    Update Conta.java

--- src/Conta.java
+++ src/Conta.java
@@ -45,8 +45,9 @@ public abstract class Conta implements IConta {
 
 	protected void imprimirInfosComuns() {
 		System.out.println(String.format("Titular: %s", this.cliente.getNome()));
-		System.out.println(String.format("Agência: %d", this.agencia));
-		System.out.println(String.format("Número: %d", this.numero));
+		System.out.println(String.format("AgÃªncia: %d", this.agencia));
+		System.out.println(String.format("NÃºmero: %d", this.numero));
 		System.out.println(String.format("Saldo: %.2f", this.saldo));
 	}
 }
+
